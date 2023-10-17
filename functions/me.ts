9e37473082962077c74ae9078f14cd8144e56052
abadia2 <abadia2@illinois.edu>
2023-10-17 00:52:56 -0500

    convert implementation of  me/ endpoint to use typscript

---
+++ functions/me.ts
@@ -0,0 +1,24 @@
+interface Env {
+  KV: KVNamespace;
+}
+
+const ME = {
+  name: "Alex Badia",
+  homepage: "https://4a2c4e9e.cloudflare-frontend-dw6.pages.dev",
+  githubURL: "https://github.com/alexbadia1",
+  interestingFact: "I'm a 'Go big or go home' type of person.",
+  skills: ["Weightlifting", "Chess", "Piano"],
+};
+
+export const onRequest: PagesFunction<Env> = async (context) => {
+  // const value = await context.env.KV.get('example');
+  const headers = new Headers();
+  headers.set("Content-Type", "application/json;charset=utf-8");
+  return new Response(
+    JSON.stringify(ME), 
+    {
+      status: 200,
+      headers: headers
+    } as ResponseInit
+  );
+};
