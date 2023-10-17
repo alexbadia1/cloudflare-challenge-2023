d17c8ac80a2159ba78f1434cfd4c8e38cae58fbb
abadia2 <abadia2@illinois.edu>
2023-10-17 16:42:04 -0500

    me/ update details about myself

--- functions/me.ts
+++ functions/me.ts
@@ -1,9 +1,9 @@
 const ME = {
   name: "Alex Badia",
   homepage: "https://4a2c4e9e.cloudflare-frontend-dw6.pages.dev",
-  githubURL: "https://github.com/alexbadia1",
-  interestingFact: "I'm a 'Go big or go home' type of person.",
-  skills: ["Weightlifting", "Chess", "Piano"],
+  githubURL: "https://github.com/alexbadia1/cloudflare-front-end",
+  interestingFact: "",
+  skills: ["Backend Development", "Cybersecurity", "Distributed Systems", "Java", "Python", "TypeScript"],
 };
 
 export const onRequestGet: PagesFunction = async (context) => {
